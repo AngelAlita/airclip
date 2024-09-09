a3307127e61ad8fd6cf23e0ed5a4d311c5a79ce9
Alita <1136142252@qq.com>
2024-09-09 21:15:45 +0800

    add idcola and cola

---
+++ scripts/ft_r32_alpha1_r32cola.sh
@@ -0,0 +1,4 @@
+CUDA_VISIBLE_DEVICES=1   python main.py  \
+    --Path IDs/ft_r32_alpha1_r32cola \
+    --save_path lora/ft_r32_alpha1_r32cola \
+    --cola
---
+++ scripts/ft_r32_alpha1_r32idcola.sh
@@ -0,0 +1,4 @@
+CUDA_VISIBLE_DEVICES=2   python main.py  \
+    --Path IDs/ft_r32_alpha1_r32idcola \
+    --save_path lora/ft_r32_alpha1_r32idcola \
+    --idcola
